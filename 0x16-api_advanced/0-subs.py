cacefe89765d9369aa217269aed3dc1215d3e2ca
root <root@localhost.localdomain>
2024-03-01 09:38:16 +0100

    task-0: return type fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,11 @@ import requests
 def number_of_subscribers(subreddit):
     """return the number of subscribers or 0"""
     n = 0
-    try:
-        r = requests.get("https://www.reddit.com/r/{}/about.json".format(
-                          subreddit),
-                         headers={'User-agent': 'ojij'},
-                         allow_redirects=False)
-        return int(json.loads(r.text)["data"]["subscribers"])
-    except (KeyError, json.decoder.JSONDecodeError):
-        return 0
+    r = requests.get("https://www.reddit.com/r/{}/about.json".format(
+                        subreddit),
+                     headers={'User-agent': 'ojij'},
+                     allow_redirects=False)
+    if r.status_code == 200:
+        return r.json()["data"]["subscribers"]
+    else:
+        return (0)
