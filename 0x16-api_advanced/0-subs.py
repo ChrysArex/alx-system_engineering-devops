1e620fb3005cafb894dc283128106b7942babe0e
root <root@localhost.localdomain>
2024-03-01 09:53:02 +0100

    task-0: return type fixed 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,18 +2,16 @@
 """the number of subscribers (not active users, total subscribers)
 for a given subreddit
 """
-import json
 import requests
 
 
 def number_of_subscribers(subreddit):
     """return the number of subscribers or 0"""
-    n = 0
     r = requests.get("https://www.reddit.com/r/{}/about.json".format(
                         subreddit),
                      headers={'User-agent': 'ojij'},
                      allow_redirects=False)
     if r.status_code == 200:
-        return r.json()["data"]["subscribers"]
-    else:
-        return (0)
+        return r.json().get("data").get("subscribers")
+    elif r.status_code == 404:
+        return 0
